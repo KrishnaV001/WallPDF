6b5abe36f98f21e1709276c25211c244bd0f4be7
krishna <kv8855588@gmail.com>
2026-08-07 01:17:52 +0530

    final

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,16 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-   
   integrations: [
     react(),
     tailwind({
       applyBaseStyles: true,
     }),
   ],
+  vite: {
+    ssr: {
+      // Prevents Vite from bundling Shiki natively and dropping its server paths
+      external: ['shiki']
+    }
+  }
 });
